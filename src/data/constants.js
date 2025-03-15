1c0f14dc6f63255b677493029e6060da2ebe1af8
varun kumar <varunkumar19999210@gmail.com>
2025-03-15 20:00:39 +0530

    update link
    cv link

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/varun-kumar99",
   resume:
-    "https://drive.google.com/file/d/1L25HnMEgvbRZHjH-8sFOymJqfJnmy1XQ/view?usp=drive_link",
+    "https://drive.google.com/file/d/16v7eymijy1QVSW8c7QYd26AMyr72fInA/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/varunkumar1999/",
   twitter: "https://x.com/Varunrajput1999",
   insta: "https://www.instagram.com/rajput.varun99/",
